9e9bf1c72f8b496363b004590659b46a13a13d92
DARSH PATEL <87805122+pateldarshhh@users.noreply.github.com>
2022-07-20 14:54:27 +0530

    Add files via upload

---
+++ sparks_bank-main/sparks_bank-main/footer.php
@@ -0,0 +1,4 @@
+<div id="container">
+    <div id="header">2022. Made by Darsh Patel <br>
+      For the Project of The Sparks Foundation</div>
+ </div>
---
+++ sparks_bank-main/sparks_bank-main/index.php
@@ -0,0 +1,23 @@
+<?php include('nav.php') ?>
+
+
+  <h1>Welcome to Sparks Foundation Bank!!!
+
+  </h1>
+  <h1>By Darsh Patel</h1>
+
+
+  <div class="bg">
+    <div class="bg">
+      <ul>
+        <li class="operations"><a href="transfer_money.php"><button class="btn" id="blue">VIEW USERS</button></a></li>
+        <li class="operations"><a href="transfer_money.php"><button class="btn" id="blue">TRANSFER AMOUNT</button></a></li>
+        <li class="operations"><a href="transactionhistory.php"><button class="btn" id="blue">TRANSFER HISTORY</button></a></li>
+      </ul>
+    </div>
+  </div>
+
+  <?php include('footer.php') ?>
+  
+</body>
+</html>
